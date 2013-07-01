1ff993d943b8973f26f32d12913432df6c3c7756
Alex Olshansky <i@creagenics.com>
2013-07-01 19:06:33 +0400

    Separate code into functions and check filename presence

--- yfd-transfer.js
+++ yfd-transfer.js
@@ -3,16 +3,26 @@
 var fs = require('fs');
 
 var fileName = process.argv[2];
+var actions = [];
 
-fs.exists(fileName, function(exists) {
-  if (!exists) {
-    return console.error('File not found');
+var readActionsLog = function() {
+  if (!fileName) {
+    console.error('Specify YFD actions log file (you can download it on http://your.flowingdata.com/actions/log/)');
+  } else {
+    fs.exists(fileName, function(exists) {
+      if (!exists) {
+        console.error('File not found');
+      } else {
+        fs.readFile(fileName, function(err, data) {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log(data.toString());
+          }
+        });
+      }
+    });
   }
-  fs.readFile(fileName, function(err, eventList) {
-    if (err) {
-      return console.error(err);
-    }
-    eventList = eventList.toString();
-    console.log(eventList);
-  });
-});
+};
+
+readActionsLog();
